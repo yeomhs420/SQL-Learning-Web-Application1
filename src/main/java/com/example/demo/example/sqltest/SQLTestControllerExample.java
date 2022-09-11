ad7946c2157b2af86a4129b096ad180a4b95ef44
jooyeokkim <jooyeok42@naver.com>
2022-09-11 21:31:21 +0900

    20220911 예제 코드 수정

--- src/main/java/com/example/demo/example/sqltest/SQLTestControllerExample.java
+++ src/main/java/com/example/demo/example/sqltest/SQLTestControllerExample.java
@@ -1,11 +1,14 @@
 package com.example.demo.example.sqltest;
 
 import com.example.demo.service.test.SQLResultService;
+import com.example.demo.validator.SQLValidator;
 import com.example.demo.vo.SQLData;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.WebDataBinder;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.InitBinder;
 import org.springframework.web.bind.annotation.PostMapping;
 
 import javax.validation.Valid;
@@ -22,11 +25,21 @@ public class SQLTestControllerExample {
         return "test/test_form";
     }
 
-//    @PostMapping("/test")
-//    public String testResult(@Valid SQLData sqlData, BindingResult result, Model model){
-//        List<Map<String, Object>> resultList = sqlResultService.getResult(sqlData.getSql());
-//        if(!sqlResultService.processResult(resultList, result, model)) return "error/sql_error";
-//        for(int i=0;i<resultList.size();i++) System.out.println(resultList.get(i));
-//        return "test/test_form";
-//    }
+    @PostMapping("/test")
+    public String testResult(@Valid SQLData sqlData, BindingResult result, Model model){
+        System.out.println("SQL : "+ sqlData.getSql());
+
+        if(!sqlResultService.checkKeywords(result, model)) return "error/sql_error";
+        List<Map<String, Object>> resultList = sqlResultService.getResult(sqlData.getSql());
+        if(!sqlResultService.checkSyntax(resultList, model)) return "error/sql_error";
+
+        System.out.println("---------------Result----------------");
+        for(int i=0;i<resultList.size();i++) System.out.println(resultList.get(i));
+        return "test/test_form";
+    }
+
+    @InitBinder
+    protected void initBinder(WebDataBinder binder) {
+        binder.setValidator(new SQLValidator());
+    }
 }
