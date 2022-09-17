3e3ffce6a1b8ac3f63a24a11fc40fd49281cd8af
chanmyeong <64055771+chanmyeong@users.noreply.github.com>
2022-09-17 17:18:44 +0900

    220917 Save Again

--- src/main/java/com/example/demo/jpa/repository/AgriFoodInfoRepository.java
+++ src/main/java/com/example/demo/jpa/repository/AgriFoodInfoRepository.java
@@ -1,2 +1,16 @@
-package com.example.demo.jpa.repository;public interface AgriFoodInfoRepository {
+package com.example.demo.jpa.repository;
+
+import com.example.demo.entity.sampledata.AgriFoodInfo;
+import com.example.demo.entity.sampledata.CovidInfectionStatus;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Date;
+import java.util.List;
+
+public interface AgriFoodInfoRepository extends JpaRepository<AgriFoodInfo, Integer> {
+    List<AgriFoodInfo> findByFood_NameLike(String galbi);
+    List<AgriFoodInfo> findByFood_CodeStartingWith(String C);
+    List<AgriFoodInfo> findByFood_VolumeBetween(float min, float max);
 }
+
+
