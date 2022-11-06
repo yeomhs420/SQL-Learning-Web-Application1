1eecb930c17adbfc444df68d66de7233373a679d
chanmyeong <64055771+chanmyeong@users.noreply.github.com>
2022-11-06 22:25:25 +0900

    221106 save temporal

--- src/main/java/com/example/demo/controller/LoginController.java
+++ src/main/java/com/example/demo/controller/LoginController.java
@@ -1,9 +1,22 @@
 package com.example.demo.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-@RequestMapping("/login")
 public class LoginController {
+
+    @GetMapping("login")
+    public String login() {
+        return "login/loginpage";
+    }
+
+    @GetMapping("signup")
+    public String signup() {
+        return "signup/signuppage";
+    }
+
 }
