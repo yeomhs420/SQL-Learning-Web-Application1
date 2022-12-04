1bf4c36894c218d1b5381a894ad5a0a1aaab4674
chanmyeong <64055771+chanmyeong@users.noreply.github.com>
2022-12-04 22:38:29 +0900

    221204 test final2

--- src/main/java/com/example/demo/jpa/repository/user/UserRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.demo.jpa.repository.user;
-
-import com.example.demo.entity.user.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<User, Long> {
-//    List<User> findAll();
-}
--- src/main/java/com/example/demo/service/LoginService.java
+++
@@ -1,8 +0,0 @@
-package com.example.demo.service;
-
-import org.springframework.stereotype.Service;
-
-@Service
-public class LoginService {
-    //유효성 검사
-}
