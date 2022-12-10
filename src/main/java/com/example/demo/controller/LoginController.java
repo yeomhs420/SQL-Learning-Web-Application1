5071338c7c7bd138552949fbca7586c00377da8a
jooyeokkim <jooyeok42@naver.com>
2022-12-11 03:06:40 +0900

    20221211 - 전체 병합 완료 2 By 팀장

--- src/main/java/com/example/demo/controller/LoginController.java
+++ src/main/java/com/example/demo/controller/LoginController.java
@@ -17,6 +17,7 @@ public class LoginController {
 
     @Autowired
     private HttpSession session;
+    // HttpSession은 Bean scope가 singleton이 아니라 session으로 자동설정 되기 때문에 @Autowired로 받아도 문제되지 않는다.
 
     @Autowired
     private LoginService loginService;
