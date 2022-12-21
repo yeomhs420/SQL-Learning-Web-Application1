41025dccda57e499a9802fa82cd4f4b35b4e2e77
jooyeokkim <jooyeok42@naver.com>
2022-12-22 02:19:53 +0900

    20221222 - could not initialize proxy - no Session 이슈 해결 By 팀장

--- src/main/java/com/example/demo/controller/TestController.java
+++ src/main/java/com/example/demo/controller/TestController.java
@@ -1,30 +1,65 @@
 package com.example.demo.controller;
 
+import com.example.demo.entity.user.User;
+import com.example.demo.jpa.repository.user.UserRepository;
 import com.example.demo.vo.Topic;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+import javax.servlet.http.HttpSession;
 import java.util.ArrayList;
 import java.util.List;
 
 @Controller
+@Transactional("userTransactionManager")
 @RequestMapping("/test")
 public class TestController {
 
     @Autowired
     List<Topic> topicList;
 
+    @Autowired
+    HttpSession session;
+
+    @Autowired
+    UserRepository userRepository;
+
     @GetMapping
-    public String list(Model model) {
+    public String list(Model model, RedirectAttributes re) {
         List<String> stateList = new ArrayList<>();
-        for(int i=0;i<17;i++) {
-            if(i%2==0) stateList.add("[미해결]");
-            else stateList.add("[해결]");
+        User user = (User)session.getAttribute("user");
+        if(user == null) {
+            re.addFlashAttribute("msg", "로그인이 필요합니다.");
+            return "redirect:/login";
         }
+        user = userRepository.findByUserId(user.getUserID()).get(0);
+        System.out.println("-----");
+        System.out.println(user);
+        System.out.println(user.getProgress());
+//        System.out.println(user.getProgress().getClass().getName());
+        System.out.println("-----");
+
+        for(int i=0;i<17;i++) {
+            user.getProgress().add(i, (user.getProgress().get(i) == null) ? false : false);
+        }
+
+        for(int i=0;i<17;i++) {
+            if(user.getProgress().get(i)==false){
+                stateList.add("[미해결]");
+            }
+            else{
+                stateList.add("[해결]");
+//                if(user.getProgress().get(i)==true) stateList.add("[해결]");
+//                else stateList.add("[미해결]");
+            }
+        }
+
         model.addAttribute("topicList", topicList);
         model.addAttribute("stateList", stateList);
         return "test/testlist";
