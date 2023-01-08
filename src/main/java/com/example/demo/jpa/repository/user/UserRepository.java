0be798ddc343d9770d0d8641d8bb03c254b46dcd
yeomhs <yeomhs420@naver.com>
2023-01-09 02:38:45 +0900

    yeom comit

--- src/main/java/com/example/demo/jpa/repository/user/UserRepository.java
+++ src/main/java/com/example/demo/jpa/repository/user/UserRepository.java
@@ -9,12 +9,10 @@ import org.springframework.transaction.annotation.Transactional;
 import java.util.List;
 
 public interface UserRepository extends JpaRepository<User, Long> {
-<<<<<<< HEAD
-    @Query(value = "SELECT * FROM User WHERE userid = :userid", nativeQuery = true)
-    List<User> findByUserId(String userid);
+
+    List<User> findByUserID(String userid);
 
 }
-=======
-    List<User> findByUserID(String userid);
-}
->>>>>>> 6618cc232f5b6986d63898274ca4db9867938b39
+
+
+
