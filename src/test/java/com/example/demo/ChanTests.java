a7c00f95eda19cea465e7e81fc11a95e673de597
jooyeokkim <jooyeok42@naver.com>
2023-01-09 00:08:18 +0900

    20230109 - merge 후 코드 정리 By 팀장

--- src/test/java/com/example/demo/ChanTests.java
+++ src/test/java/com/example/demo/ChanTests.java
@@ -1,6 +1,7 @@
 package com.example.demo;
 
 import com.example.demo.entity.user.User;
+import com.example.demo.jpa.repository.user.UserRepository;
 import com.example.demo.service.sampledata.CovidVaccinationCenterService;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -14,6 +15,9 @@ import org.springframework.test.web.servlet.MockMvc;
 
 import javax.servlet.http.HttpSession;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
@@ -24,6 +28,9 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 public class ChanTests {
     @Autowired
     MockMvc mockMvc;
+
+    @Autowired
+    UserRepository userRepository;
 
     @Test
     public void TestController_list() throws Exception {
@@ -47,9 +54,22 @@ public class ChanTests {
         int question2 = 4;
         int question3 = 3;
         String body = "{\"unit\":"+unit+", \"question1\": "+question1+", \"question2\": "+question2+", \"question3\": \""+question3+"\"}";
+
+        User user = new User();
+        user.setId(1000);
+        user.setUserID("jooyeok");
+        user.setUserPassword("!wndur0703");
+        user.setUserName("김주역");
+        user.setUserEmail("jooyeok42@naver.com");
+        List<Boolean> progress = new ArrayList<>();
+        for (int i = 0; i < 17; i++) progress.add(false);
+        user.setProgress(progress);
+        userRepository.save(user);
+
         mockMvc.perform(post("/test/grading")
-                .content(body)
-                .contentType(MediaType.APPLICATION_JSON))
+                        .content(body)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .sessionAttr("user", user))
                 .andExpect(status().isOk())
                 .andExpect(jsonPath("$.questionList[0].isCorrect").value(true))
                 .andExpect(jsonPath("$.questionList[1].isCorrect").value(true))
